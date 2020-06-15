b5d758dd3e8669a68aa168882458c3730c19ddce
Douglas Duteil <douglasduteil@gmail.com>
2020-06-15 21:04:32 +0200

    test: use path in tests (2)

--- src/fs/clone.test.ts
+++ src/fs/clone.test.ts
@@ -1,4 +1,5 @@
 import { copy } from "fs-extra";
+import { join } from "path";
 
 import { cloneFolders } from "./clone";
 
@@ -7,5 +8,5 @@ jest.mock("fs-extra");
 test.concurrent("clone the qux", async () => {
   const cloneFromFooToBar = cloneFolders("foo", "bar");
   await cloneFromFooToBar("qux");
-  expect(copy).toHaveBeenCalledWith("foo/qux", "bar/qux");
+  expect(copy).toHaveBeenCalledWith(join("foo/qux"), join("bar/qux"));
 });
